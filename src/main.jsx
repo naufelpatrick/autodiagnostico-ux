30afbb8ae2a868fd0142db23ca59e9750627af29
naufelpatrick <naufelpatrick@gmail.com>
2026-05-26 12:09:43 -0300

    ccs-dark

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 document.documentElement.classList.add('dark');
+document.documentElement.style.colorScheme = 'dark';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
