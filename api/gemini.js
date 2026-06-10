26af3f19d2e7c95a82e3994f38c4c7288a7cb3c3
patricknaufel-foton <patrick.naufel@foton.la>
2026-06-10 09:49:25 -0300

    Log Modelo de Uso Gemini

--- api/gemini.js
+++ api/gemini.js
@@ -13,6 +13,8 @@ export default async function handler(req, res) {
     const apiKey = process.env.GEMINI_API_KEY;
     const modelName = "gemini-2.0-flash-lite";
 
+    console.log("MODEL USADO:", modelName);
+
     const response = await fetch(
       `https://generativelanguage.googleapis.com/v1beta/models/${modelName}:generateContent?key=${apiKey}`,
       {
